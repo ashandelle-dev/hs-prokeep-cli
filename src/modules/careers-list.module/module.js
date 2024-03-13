27e36ddd89e24134979a33e361e3f6722607a6e6
Bobbie Wilson <bobbiewilson@MacBook-Pro-2.local>
2024-03-13 00:24:52 -0500

    Fixed malformed HTML of careers module blocking rendering

--- src/modules/careers-list.module/module.js
+++ src/modules/careers-list.module/module.js
@@ -10,12 +10,14 @@ var settings = {
 
 $.ajax(settings).done(function (response) {
   const departments = [];
-  console.log(response);
+  //console.log(response);
+  console.log('received');
   response = response.sort((a, b) => a.posted_at > b.posted_at ? -1 : 1);
   response = response.sort((a, b) => a.rippling_department > b.rippling_department ? -1 : 1);
   
   $.each(response, function(){
-    // console.log({response});
+    
+    console.log({response});
     if(this.active != false){
       departments.push(this.rippling_department);
       if(this.city != 'Remote') {
@@ -23,22 +25,25 @@ $.ajax(settings).done(function (response) {
       } else {
         stateAbbrev = '';
       }
-    $('.response')
-      .append(`
-      <div class="listing mb-6 shadow bg-white rounded-lg p-6">
-        <div class="flex justify-center md:justify-between items-center flex-wrap w-full " >
-          <div class="mb-3 md:mb-0 text-center md:text-left w-full md:w-3/5">
-            
-            <p class="m-0 text-[24px] font-bold">${this.title}</p>
-            <p class="m-0">${this.rippling_department}</p>
-           
+      
+
+      $('.response')
+        .append(`
+        <div class="listing mb-6 shadow bg-white rounded-lg p-6">
+          <div class="flex justify-center md:justify-between items-center flex-wrap w-full " >
+            <div class="mb-3 md:mb-0 text-center md:text-left w-full md:w-3/5">
+              
+              <p class="m-0 text-[24px] font-bold">${this.title}</p>
+              <p class="m-0">${this.rippling_department}</p>
+             
+            </div>
+            <div class="w-full md:w-2/5 flex justify-center md:justify-end">
+             <a class="btn btn-primary mx-auto md:mx-0 px-6 py-2" target="_blank" href="${this.joblink}">See details & apply</a>
+            </div>
+         
           </div>
-          <div class="w-full md:w-2/5 flex justify-center md:justify-end">
-           <a class="btn btn-primary mx-auto md:mx-0 px-6 py-2" target="_blank" href="${this.joblink}">See details & apply</a>
-          <div>
-       
-        </a>
-      </div>`);
+        </div>`);
+      
     }
   });
 });
