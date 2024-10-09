1babcbc4fc5a8eccae2def879d469e94cb6104e6
Stephanie O'Gay Garcia <stephanieogaygarcia@stephaneslaptop.home>
2024-10-09 11:29:09 -0400

    Fixes to Tabbed Sticky Scroller module

--- src/modules/tabbed-sticky-scroller.module/module.js
+++ src/modules/tabbed-sticky-scroller.module/module.js
@@ -1,71 +1,73 @@
-// Intersection observer
-function createIntersectionObserver(
-  callback, 
-  opts = { 
-    root: null, // viewport
-    rootMargin: '0px', 
-    threshold: 0.5 // at 50% visible
-  }) {
-    var previousY = new Map();
-    var observer = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function (entry) {
-        var currY = entry.boundingClientRect.y;
-        var prevY = previousY.get(entry.target);
-        if ( currY < prevY ) { /* scroll down */ entry.scrollDirectionY = 'down'; }
-        if ( currY > prevY ) { /* scroll up */ entry.scrollDirectionY = 'up'; }
-        callback(entry, observer, entries);
-        previousY.set(entry.target, currY);
-      });
-    }, opts)
-  
-    return observer;
-}
+window.addEventListener("load", (event) => {
+  // Intersection observer
+  function createIntersectionObserver(
+    callback, 
+    opts = { 
+      root: null, // viewport
+      rootMargin: '0px', 
+      threshold: 0.5 // at 50% visible
+    }) {
+      var previousY = new Map();
+      var observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function (entry) {
+          var currY = entry.boundingClientRect.y;
+          var prevY = previousY.get(entry.target);
+          if ( currY < prevY ) { /* scroll down */ entry.scrollDirectionY = 'down'; }
+          if ( currY > prevY ) { /* scroll up */ entry.scrollDirectionY = 'up'; }
+          callback(entry, observer, entries);
+          previousY.set(entry.target, currY);
+        });
+      }, opts)
+    
+      return observer;
+  }
 
-// Observer
-var observer = createIntersectionObserver(function(entry){
-    const sectionNumber = entry.target.dataset.sectionNumber;
-    const sectionImage = document.getElementById('stickyScrollerImage' + sectionNumber);
-    const sectionTab = document.getElementById('stickyScrollerTab' + sectionNumber);
-    const sectionLottie = document.getElementById('stickyScrollerLottie' + sectionNumber);
-    if (sectionLottie) {
-      const sectionLottiePlayer = sectionLottie.getLottie();
-    }
-    const numberOfSections = entry.target.dataset.numberOfSections;
-    if (entry.isIntersecting) {
-      sectionImage.classList.remove('is-hidden');
-      sectionTab.classList.add('is-selected');
+  // Observer
+  var observer = createIntersectionObserver(function(entry){
+      var sectionNumber = entry.target.dataset.sectionNumber;
+      var sectionImage = document.getElementById('stickyScrollerImage' + sectionNumber);
+      var sectionTab = document.getElementById('stickyScrollerTab' + sectionNumber);
+      var sectionLottie = document.getElementById('stickyScrollerLottie' + sectionNumber);
       if (sectionLottie) {
-        sectionLottiePlayer.goToAndPlay(0,false);
+        var sectionLottiePlayer = sectionLottie.getLottie();
       }
-    }
-    else {
-      if ((sectionNumber === "1") && (entry.scrollDirectionY == 'up')) { 
-        // scrolling up past first section
+      var numberOfSections = entry.target.dataset.numberOfSections;
+      if (entry.isIntersecting) {
         sectionImage.classList.remove('is-hidden');
         sectionTab.classList.add('is-selected');
         if (sectionLottie) {
           sectionLottiePlayer.goToAndPlay(0,false);
         }
-      } else if ((sectionNumber === numberOfSections) && (entry.scrollDirectionY == 'down')) { 
-        // scrolling down past last section
-        sectionImage.classList.remove('is-hidden');
-        sectionTab.classList.add('is-selected');
-        if (sectionLottie) {
-          sectionLottiePlayer.goToAndPlay(0,false);
-        }
-      } else {
-        sectionImage.classList.add('is-hidden');
-        sectionTab.classList.remove('is-selected');
       }
-    }
-});
-  
-document.querySelectorAll('.sticky-scroller__section').forEach(function(el) { observer.observe(el); });
+      else {
+        if ((sectionNumber === "1") && (entry.scrollDirectionY == 'up')) { 
+          // scrolling up past first section
+          sectionImage.classList.remove('is-hidden');
+          sectionTab.classList.add('is-selected');
+          if (sectionLottie) {
+            sectionLottiePlayer.goToAndPlay(0,false);
+          }
+        } else if ((sectionNumber === numberOfSections) && (entry.scrollDirectionY == 'down')) { 
+          // scrolling down past last section
+          sectionImage.classList.remove('is-hidden');
+          sectionTab.classList.add('is-selected');
+          if (sectionLottie) {
+            sectionLottiePlayer.goToAndPlay(0,false);
+          }
+        } else {
+          sectionImage.classList.add('is-hidden');
+          sectionTab.classList.remove('is-selected');
+        }
+      }
+  });
+    
+  document.querySelectorAll('.sticky-scroller__section').forEach(function(el) { observer.observe(el); });
 
-const sectionTabs = document.querySelectorAll('.sticky-scroller__tab');
-sectionTabs.forEach((sectionTab) => {
-  sectionTab.addEventListener('click', function(){
-    var scrollTargetID = sectionTab.dataset.target;
-    document.querySelector('[data-section-number="'+scrollTargetID+'"]').scrollIntoView({behavior: 'smooth', block: 'start'});
+  const sectionTabs = document.querySelectorAll('.sticky-scroller__tab');
+  sectionTabs.forEach((sectionTab) => {
+    sectionTab.addEventListener('click', function(){
+      var scrollTargetID = sectionTab.dataset.target;
+      document.querySelector('[data-section-number="'+scrollTargetID+'"]').scrollIntoView({behavior: 'smooth', block: 'start'});
+    });
   });
 });
